efcd5039d75a6a6b040ffec3189b3c51055f9ee1
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-09-01 12:19:26 +0200

    Add registry and repository ID as outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
 # ------------------------------------------------------------------------------
 # Output
 # ------------------------------------------------------------------------------
+output "id" {
+  description = "ID of the repository."
+  value       = aws_ecr_repository.ecr_repo.id
+}
+
 output "arn" {
   description = "Full ARN of the repository."
   value       = aws_ecr_repository.ecr_repo.arn
@@ -9,4 +14,9 @@ output "arn" {
 output "url" {
   description = "The URL of the repository (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName )"
   value       = aws_ecr_repository.ecr_repo.repository_url
+}
+
+output "registry_id" {
+  description = "ID of the registry."
+  value       = aws_ecr_repository.ecr_repo.registry_id
 }
